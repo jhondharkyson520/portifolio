89595311a75be8fad69285efe009ec7bb5f61b7b
devjhon <jhondharkyson@gmail.com>
2023-08-09 13:39:36 -0300

    Atualização cards projetos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import JmIcon from './assets/JmIcon.png'
 import Computing01 from './assets/Computing01.jpg'
+import { SiTypescript } from 'react-icons/si'
 import './App.css'
-import {FaGithub, FaWhatsapp, FaLinkedin} from 'react-icons/fa'
+import {FaGithub, FaWhatsapp, FaLinkedin, FaEye, FaHtml5, FaCss3, FaSass, FaReact} from 'react-icons/fa'
 
 function App() {
   
@@ -46,44 +47,213 @@ function App() {
 
             </nav>
         </header>
-        <div className='about'>
-          <h1>Sobre</h1>
-          <p>
-            Olá, me chamo Jhon Mendonça, sou <strong>desenvolvedor Front-End</strong> formado em Sistemas de informação pela 
-            UEMG - Universidade do Estado de Minas Gerais, Pós Graduado em Projetos de Cloud Computing e 
-            Projetos de Aplicativos Móveis Multiplataforma pela UNIAMERICA. Tenho experiência em 
-            <strong> HTML5, CSS3, TypeScript, ReactJS e React Native. </strong> 
-             Abaixo disponibilizei alguns projetos desenvolvidos por mim. 
-          </p>
-        </div>
+        
+        <div className='container-about'>
+          <div className='about'>
+            <h1>Sobre</h1>
+            <p>
+              Olá, me chamo Jhon Mendonça, sou <strong>desenvolvedor Front-End</strong> formado em Sistemas de informação pela 
+              UEMG - Universidade do Estado de Minas Gerais, Pós Graduado em Projetos de Cloud Computing e 
+              Projetos de Aplicativos Móveis Multiplataforma pela UNIAMERICA. Tenho experiência em 
+              <strong> HTML5, CSS3, TypeScript, ReactJS e React Native. </strong> 
+              Abaixo disponibilizei alguns projetos desenvolvidos por mim. 
+            </p>
+          </div>
 
-        <div className='imagem01'>
-          <img 
-            src={Computing01} 
-            alt="Dev codes"
-            className='computing01' 
-          />
+          <div className='imagem01'>
+            <img 
+              src={Computing01} 
+              alt="Dev codes"
+              className='computing01' 
+            />
+          </div>
         </div>
         
         <div className='projetos'>
           <div className='projeto01'>
-              Barber
+              <h1>Barbearia</h1>
+              <p>Landing page de apresentação de uma barbearia.</p>
+
+              <a href="https://projeto-barberdev-glij.vercel.app/" target='_blank'>
+                <button className='button-barber'>
+                  Visualizar <FaEye size={20}/>
+                </button>
+              </a>
+
+              <a href="https://github.com/jhondharkyson520/projeto-barberdev" target='_blank'>
+                <button className='button-barber-github'>
+                  GitHub <FaGithub size={20}/>
+                </button>
+              </a>
+
+              <br />
+              <br />
+              <p><strong>Tecnologias utilizadas: </strong></p>
+              <br />
+              <p>
+                <strong>
+                HTML5 <FaHtml5 style={{color: '#ec6231'}}/> 
+                <br />
+                CSS3 <FaCss3 style={{color: '#264de4'}}/> 
+                <br />
+                Sass <FaSass size={20} style={{color: '#cc6699'}}/>
+                <br /> 
+                GitHub <FaGithub/>
+                <br />
+                </strong>
+              </p>
           </div>
 
           <div className='projeto02'>
-              Sorveteria
+            <h1>Sorveteria</h1>
+          <p>Desenvolver</p>
+
+              <a href="#" target='_blank'>
+                <button className='button-barber'>
+                  Visualizar <FaEye size={20}/>
+                </button>
+              </a>
+
+              <a href="#" target='_blank'>
+                <button className='button-barber-github'>
+                  GitHub <FaGithub size={20}/>
+                </button>
+              </a>
+
+              <br />
+              <br />
+              <p><strong>Tecnologias utilizadas: </strong></p>
+              <br />
+              <p>
+                <strong>
+                HTML5 <FaHtml5 style={{color: '#ec6231'}}/> 
+                <br />
+                CSS3 <FaCss3 style={{color: '#264de4'}}/> 
+                <br />
+                Sass <FaSass size={20} style={{color: '#cc6699'}}/>
+                <br /> 
+                TypeScript <SiTypescript style={{color: '#007acc'}}/>
+                <br />
+                ReactJS <FaReact size={20} style={{color: '#61dbfb'}}/>
+                <br />
+                GitHub <FaGithub/>
+                <br />
+                </strong>
+              </p>
           </div>
 
           <div className='projeto03'>
-              Reserva
+            <h1>Reserva</h1>
+          <p>Desenvolver</p>
+
+              <a href="#" target='_blank'>
+                <button className='button-barber'>
+                  Visualizar <FaEye size={20}/>
+                </button>
+              </a>
+
+              <a href="#" target='_blank'>
+                <button className='button-barber-github'>
+                  GitHub <FaGithub size={20}/>
+                </button>
+              </a>
+
+              <br />
+              <br />
+              <p><strong>Tecnologias utilizadas: </strong></p>
+              <br />
+              <p>
+                <strong>
+                HTML5 <FaHtml5 style={{color: '#ec6231'}}/> 
+                <br />
+                CSS3 <FaCss3 style={{color: '#264de4'}}/> 
+                <br />
+                Sass <FaSass size={20} style={{color: '#cc6699'}}/>
+                <br /> 
+                TypeScript <SiTypescript style={{color: '#007acc'}}/>
+                <br />
+                ReactJS <FaReact size={20} style={{color: '#61dbfb'}}/>
+                <br />
+                GitHub <FaGithub/>
+                <br />
+                </strong>
+              </p>
           </div>
 
-          <div className='projeto04'>
-              Finanças
+          <div className='projeto02'>
+            <h1>Finanças</h1>
+          <p>Desenvolver</p>
+
+              <a href="#" target='_blank'>
+                <button className='button-barber'>
+                  Visualizar <FaEye size={20}/>
+                </button>
+              </a>
+
+              <a href="#" target='_blank'>
+                <button className='button-barber-github'>
+                  GitHub <FaGithub size={20}/>
+                </button>
+              </a>
+
+              <br />
+              <br />
+              <p><strong>Tecnologias utilizadas: </strong></p>
+              <br />
+              <p>
+                <strong>
+                HTML5 <FaHtml5 style={{color: '#ec6231'}}/> 
+                <br />
+                CSS3 <FaCss3 style={{color: '#264de4'}}/> 
+                <br />
+                Sass <FaSass size={20} style={{color: '#cc6699'}}/>
+                <br /> 
+                TypeScript <SiTypescript style={{color: '#007acc'}}/>
+                <br />
+                ReactJS <FaReact size={20} style={{color: '#61dbfb'}}/>
+                <br />
+                GitHub <FaGithub/>
+                <br />
+                </strong>
+              </p>
           </div>
 
-          <div className='projeto05'>
-              Vendas
+          <div className='projeto02'>
+            <h1>Vendas</h1>
+          <p>Desenvolver</p>
+
+              <a href="#" target='_blank'>
+                <button className='button-barber'>
+                  Visualizar <FaEye size={20}/>
+                </button>
+              </a>
+
+              <a href="#" target='_blank'>
+                <button className='button-barber-github'>
+                  GitHub <FaGithub size={20}/>
+                </button>
+              </a>
+
+              <br />
+              <br />
+              <p><strong>Tecnologias utilizadas: </strong></p>
+              <br />
+              <p>
+                <strong>
+                HTML5 <FaHtml5 style={{color: '#ec6231'}}/> 
+                <br />
+                CSS3 <FaCss3 style={{color: '#264de4'}}/> 
+                <br />
+                Sass <FaSass size={20} style={{color: '#cc6699'}}/>
+                <br /> 
+                TypeScript <SiTypescript style={{color: '#007acc'}}/>
+                <br />
+                ReactJS <FaReact size={20} style={{color: '#61dbfb'}}/>
+                <br />
+                GitHub <FaGithub/>
+                <br />
+                </strong>
+              </p>
           </div>
         </div>
     </div>
