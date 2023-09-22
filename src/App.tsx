7c14e134f2da32373d7f98dd7515391d2fc00a5a
jhondharkyson <jhondharkyson@gmail.com>
2023-09-22 20:40:56 -0300

    Atualização de projetos

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Computing01 from './assets/Computing01.jpg'
 import { SiTypescript } from 'react-icons/si'
 import './App.css'
 import {FaGithub, FaWhatsapp, FaLinkedin, FaEye, FaHtml5, FaCss3, FaSass, FaReact} from 'react-icons/fa'
+import {BiLogoFirebase, BiLogoTailwindCss} from 'react-icons/bi'
 
 function App() {
   
@@ -172,6 +173,45 @@ function App() {
                 TypeScript <SiTypescript style={{color: '#007acc'}}/>
                 <br />
                 ReactJS <FaReact size={20} style={{color: '#61dbfb'}}/>
+                <br />
+                GitHub <FaGithub/>
+                <br />
+                </strong>
+              </p>
+          </div>
+
+          <div className='projeto01'>
+            <h1>Borracharia</h1>
+            <br />
+            <p>Aplicação web vendida para uma borracharia, em produção.</p>
+
+              <a href="https://cripto-umber.vercel.app/" target='_blank'>
+                <button className='button-barber'>
+                  Visualizar <FaEye size={20}/>
+                </button>
+              </a>
+
+              <a href="https://github.com/jhondharkyson520/cripto" target='_blank'>
+                <button className='button-barber-github'>
+                  GitHub <FaGithub size={20}/>
+                </button>
+              </a>
+
+              <br />
+              <br />
+              <p><strong>Tecnologias utilizadas: </strong></p>
+              <br />
+              <p>
+                <strong>
+                HTML5 <FaHtml5 style={{color: '#ec6231'}}/> 
+                <br />
+                Tailwind Css <BiLogoTailwindCss style={{color: '#264de4'}}/> 
+                <br />
+                TypeScript <SiTypescript style={{color: '#007acc'}}/>
+                <br />
+                ReactJS <FaReact size={20} style={{color: '#61dbfb'}}/>
+                <br />
+                Firebase <BiLogoFirebase size={20} style={{color: '#F5820C'}}/>
                 <br />
                 GitHub <FaGithub/>
                 <br />
